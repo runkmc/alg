6c1d64f57e5beafcd7a281d2edf84df14e4a49c6
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-05-23 18:52:16 -0700

    Remove Haskell stuff

--- haskell/sorting.hs
+++
@@ -1,6 +0,0 @@
-quicksort :: (Ord a) => [a] -> [a]
-quicksort [] = []
-quicksort (x:xs) =
-    let smallOrEqual = filter (<=x) xs
-        larger = filter (>x) xs
-    in  quicksort smallOrEqual ++ [x] ++ quicksort larger
