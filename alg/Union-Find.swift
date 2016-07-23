bc002ebd9b56acac733cf250189170eed9afadb4
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-07-22 23:24:16 -0700

    Union-Find algorithm passes initial test

--- alg/Union-Find.swift
+++ alg/Union-Find.swift
@@ -1,2 +1,47 @@
 import Foundation
 import XCTest
+
+class UnionFind {
+    var nodes: [Int]
+    var size: [Int]
+    
+    init(count:Int) {
+        self.nodes = Array(repeating: 0, count: count)
+        for index in 0..<count {
+            nodes[index] = index
+        }
+        self.size = Array(repeating: 1, count: count)
+    }
+    
+    func connected(_ firstNode:Int, _ secondNode:Int) -> Bool {
+        return root(node: firstNode) == root(node: secondNode)
+    }
+    
+    func connect(_ firstNode:Int, _ secondNode:Int) {
+        guard !connected(firstNode, secondNode) else { return }
+        
+        if size[firstNode] > size[secondNode] {
+            nodes[secondNode] = firstNode
+            size[firstNode] += size[secondNode]
+            return
+        }
+        
+        nodes[firstNode] = secondNode
+        size[secondNode] += size[firstNode]
+    }
+    
+    private func root(node:Int) -> Int {
+        var current = node
+        while nodes[current] != current { current = nodes[current] }
+        return current
+    }
+}
+
+class UnionFindTest: XCTestCase {
+    func testConnections() {
+        let uf = UnionFind(count: 11)
+        uf.connect(3, 4)
+        uf.connect(4, 8)
+        XCTAssertTrue(uf.connected(3, 8))
+    }
+}
